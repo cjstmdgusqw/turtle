99837a0be633dd29a690d670f603926e2cab8d2b
koon <cjstmdgusqw@gmail.com>
2022-10-09 14:54:33 +0900

    new html dinner

--- articles/views.py
+++ articles/views.py
@@ -1,3 +1,16 @@
+import random
 from django.shortcuts import render
 
-# Create your views here.
+# Create your views here. #제일많이 작업하는곳
+def index(request):
+    return render(request, 'index.html')
+
+def dinner(request, name):
+    menus = [{"name" : "족발", "price" : 30000},{"name" : "치킨", "price" : 18000},{"name" : "피자", "price" : 13000},{"name" : "초밥", "price" : 21000},]
+    pick = random.choice(menus)
+    context = {
+        'pick' : pick,
+        'name' : name,
+        'menus': menus
+    }
+    return render(request, 'dinner.html', context)    
