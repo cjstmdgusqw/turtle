04edd2cf1a261dd165496b65fb5776095cb9bffa
koon <cjstmdgusqw@gmail.com>
2022-10-10 12:32:45 +0900

    add new models articles

--- articles/models.py
+++ articles/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length = 10)
+    content = models.TextField()
+    create_at = models.DateTimeField(auto_now_add = True) # 생성시에 자동으로 추가가 되겠다 라는 뜻임
+
+
